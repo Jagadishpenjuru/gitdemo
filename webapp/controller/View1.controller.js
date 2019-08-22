064fc0bcb7ef00ad0c888bfbabef471c109bdc2e
jagadishpenjuru <jagadish.penjuru@gmail.com>
2019-08-22 09:55:44 +0000

    Commit changes

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("SAPUI5.demoUI5.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"SAPUI5/demoUI5/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
